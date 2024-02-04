8e3f451e96c4e7f5f805651a3452cfd7cd9ed11e
Vinay-Bagde-19 <vinaybagde19@gmail.com>
2024-02-04 20:57:54 +0530

    anime website

---
+++ app.js
@@ -0,0 +1,75 @@
+//SCROLL HEADER
+function scrollHeader() {
+  const header = document.getElementById("header");
+  if(this.scrollY >=50) header.classList.add('scroll-header');
+  else header.classList.remove('scroll-header');
+}
+
+window.addEventListener("scroll",scrollHeader);
+
+
+//brand spiner
+var brandSwiper = new Swiper(".brand-swiper", {
+    spaceBetween: 24,
+    loop: true,
+    breakpoints: {
+     450: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+          },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 24,
+      },
+      1200: {
+        slidesPerView: 5,
+        spaceBetween: 24,
+      },
+    },
+  });
+
+//Anime Swiper
+var AnimeSwiper = new Swiper(".anime-swiper", {
+  spaceBetween: 24,
+  loop: true,
+  breakpoints: {
+   576: {
+          slidesPerView: 2,
+          spaceBetween: 24,
+        },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 24,
+    },
+    1400: {
+      slidesPerView: 5,
+      spaceBetween: 24,
+    },
+  },
+});
+
+
+//Show Menu
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+if(navToggle){
+  navToggle.addEventListener("click", () =>{
+    navMenu.classList.add("show-menu");
+  });
+}
+
+if(navClose){
+  navClose.addEventListener("click", () =>{
+    navMenu.classList.remove("show-menu");
+  });
+}
